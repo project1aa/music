b56fb2d97b3e7703bc622dfbe601b584149c4bfd
= <=>
2018-09-25 19:52:18 +0330

    Added latest_songs sections to base.html and views.py

--- music/views.py
+++ music/views.py
@@ -7,6 +7,12 @@ from .models import Song
 class SongListView(ListView):
     model = Song
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        latest_songs = Song.objects.all()[:10]
+        context['latest_songs'] = latest_songs
+        return context
+
 
 class SongDetailView(DetailView):
     model = Song
